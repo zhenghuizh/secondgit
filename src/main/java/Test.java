20d363cad89033d65be25175fe81d87f19c43b4c
zh <960235604@qq.com>
2018-04-27 16:45:26 +0800

    大郑慧的第二次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -4,5 +4,6 @@ public class Test {
         System.out.println("使用idea集成git");
         System.out.println("眼睛疼啊眼睛疼");
         System.out.println("大郑慧的另一次提交");
+        System.out.println("大郑慧的第二次提交");
     }
 }
